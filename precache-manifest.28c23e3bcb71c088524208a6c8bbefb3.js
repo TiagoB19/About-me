aa0de43632e1d5a2e35ca7423cdd53eafda7a8c9
Tiago Borges <borgestiago13@gmail.com>
2020-06-26 19:56:05 -0300

    Updates

---
+++ precache-manifest.28c23e3bcb71c088524208a6c8bbefb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca78a065df791cc947a984589417a72",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "b6fd4a7aa65c6a5b3255",
+    "url": "/about-me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea423dfba40c9c1aaa2d",
+    "url": "/about-me/static/js/2.1e77b4a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/about-me/static/js/2.1e77b4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6fd4a7aa65c6a5b3255",
+    "url": "/about-me/static/js/main.2c3e6501.chunk.js"
+  },
+  {
+    "revision": "10194b59d949a1ff5a74",
+    "url": "/about-me/static/js/runtime-main.3bf9f858.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about-me/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a86a79987a84ca36768cc6471902e7be.js"
+  "/about-me/precache-manifest.28c23e3bcb71c088524208a6c8bbefb3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/about-me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
