a0339d7cbff3795dbc682b10083aa61b9112fe44
Tiago Borges <borgestiago13@gmail.com>
2020-06-26 19:59:31 -0300

    1.0

--- src/App.js
+++ src/App.js
@@ -7,17 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Página em construção! Em breve no ar.</p>
+        <h5>by Tiago Borges Nascimento</h5>
       </header>
     </div>
   );
